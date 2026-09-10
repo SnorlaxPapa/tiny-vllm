6750879cfa07aa9a6fa2833a17df872009b17f7c
Ye Rongyang <snorlaxdaddy@Yes-MacBook-Air.local>
2026-09-10 10:19:31 -0700

    added add to sequence

--- nanovllm/engine/sequence.py
+++ nanovllm/engine/sequence.py
@@ -30,6 +30,10 @@ class Sequence:
         self.ignore_eos = sampling_params.ignore_eos
 
 
+    def add(self, seq: Sequence):
+        self.waiting.append(seq)
+        
+
     def __len__(self):
         return len(self.token_ids)
 
