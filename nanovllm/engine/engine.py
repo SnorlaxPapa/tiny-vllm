4025051a8d288b7e9c31f51389513be8e136d135
Ye Rongyang <yyeoy@gmail.com>
2026-09-11 04:46:44 +0000

    Added benchmarking features to engine

--- nanovllm/engine/engine.py
+++ nanovllm/engine/engine.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer
+from time import perf_counter
 from nanovllm.engine.model_runner import ModelRunner
 from nanovllm.engine.scheduler import Scheduler
 from nanovllm.engine.sequence import Sequence, SequenceStatus
@@ -7,7 +8,7 @@ from nanovllm.layers.sampler import Sampler
 
 class EngineCore:
 
-  def __init__(self, model_dir):
+  def __init__(self, model_dir: str, benchmark: bool = False):
     self.runner = ModelRunner(model_dir) #allocate kv cache 
     config = self.runner.config
     self.tokenizer = AutoTokenizer.from_pretrained(model_dir)
@@ -15,16 +16,32 @@ class EngineCore:
     self.scheduler = Scheduler(config)
     self.request_ids = []
     self.results = {}
-
+    self.benchmark = benchmark
+    
+    #calculate tft for benchmark
+    if self.benchmark:
+      self.start_times = {}
+      self.ttft = {}
+      self.first_token_times = {}
+      self.last_token_times = {}
+      self.itls = []
 
   def add_prompt(self, input: str, sampling_param: SamplingParams):
+    if self.benchmark: 
+      start_time = perf_counter()
+
     token_ids = self.tokenizer.apply_chat_template(
-      [{"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+      [{"role": "system", "content": "/no_think You are Qwen, a helpful assistant."},
        {"role": "user", "content": input}],
       tokenize=True,
       add_generation_prompt=True,
     )
+
     sequence = Sequence(token_ids["input_ids"], sampling_param)
+    
+    if self.benchmark:
+      self.start_times[sequence.seq_id] = start_time
+
     self.request_ids.append(sequence.seq_id)
     self.scheduler.add(sequence)
 
@@ -39,6 +56,14 @@ class EngineCore:
     else:
       sampling_params = [sampling_params] * len(inputs)
     
+    #reset our accumulators
+    if self.benchmark:
+      self.start_times.clear()
+      self.ttft.clear()
+      self.first_token_times.clear()
+      self.last_token_times.clear()
+      self.itls.clear()
+    
     self.request_ids = []
     self.results = {}
     #add prompts to waiting
@@ -51,6 +76,32 @@ class EngineCore:
 
       outputs = self.runner.run(scheduled_sequences, is_prefill)
 
+      #get ttft for benchmark
+      if self.benchmark:
+        end_time = perf_counter()
+        for sequence in scheduled_sequences:
+
+          finished_prefill = (
+              sequence.num_computed_tokens + 
+              sequence.num_scheduled_tokens ==
+              len(sequence)
+          )
+
+          produces_token = (
+              not is_prefill or finished_prefill
+          )
+
+          if not produces_token: continue
+
+          if sequence.seq_id not in self.first_token_times:
+            self.first_token_times[sequence.seq_id] = end_time
+            self.ttft[sequence.seq_id] = end_time - self.start_times[sequence.seq_id]
+          
+          else:
+            self.itls.append(end_time - self.last_token_times[sequence.seq_id])
+          
+          self.last_token_times[sequence.seq_id] = end_time
+
       #postprocessing scheduled sequences
       self.scheduler.postprocess(scheduled_sequences, outputs, is_prefill)
 
@@ -59,6 +110,12 @@ class EngineCore:
           output = self.tokenizer.decode(sequence.token_ids[sequence.num_prompt_tokens:], skip_special_tokens=True)
           self.results[sequence.seq_id] = output
 
+    if self.benchmark:
+      #get average itl and ttft
+      average_ttft = sum(self.ttft.values()) / len(self.ttft)
+      average_itl = sum(self.itls) / len(self.itls) if self.itls else None
+
+      return average_ttft, average_itl
 
     return [self.results[seq] for seq in self.request_ids]
 
